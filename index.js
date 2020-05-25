507fbf2c34ec092cd13f2d63fa4bcef11b0a6e3b
Dominique DUPLAN <dominique.duplan@batiactugroupe.com>
2020-05-25 13:44:57 +0200

    ajout de la fonction getAllProducts

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 let fs = require("fs")
 let path = require("path")
 
-fs.readFile(path.join(__dirname,"products.json"), "utf8", (err,contents) => {
-    if (err) return console.log('Erreur de lecture du fichier poducts : ', err)
-    try {
-        var products = JSON.parse(contents).products ;    
-        console.log(products)
-    } catch (error) {
-        console.log('Erreur parking du fichier products')
-    }   
-})
+function getAllProducts() {
+    fs.readFile(path.join(__dirname,"products.json"), "utf8", (err,contents) => {
+        if (err) return console.log('Erreur de lecture du fichier poducts : ', err)
+        try {
+            var products = JSON.parse(contents).products ; 
+            console.log("Bienvenue. Voici les produits disponibles")   
+            products.forEach(product => {
+                console.log(product.id, "-",product.name,"/",product.EUR_price/100,"/",product.orders_counter)
+            });
+        } catch (error) {
+            console.log('Erreur parking du fichier products')
+        }   
+    })
+}
+
+getAllProducts();
