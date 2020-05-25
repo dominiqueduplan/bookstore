1a2350437f06238f90ad1a5f464b674c0317eaa0
Dominique DUPLAN <dominique.duplan@batiactugroupe.com>
2020-05-25 14:34:01 +0200

    ajout de orderProductById avec input stdin

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 let fs = require("fs")
 let path = require("path")
+let readline = require('readline')
 
-function getAllProducts() {
+function getAllProducts(callback) {
     fs.readFile(path.join(__dirname,"products.json"), "utf8", (err,contents) => {
-        if (err) return console.log('Erreur de lecture du fichier poducts : ', err)
+        if (err) return callback(err)
         try {
             var products = JSON.parse(contents).products ; 
             console.log("Bienvenue. Voici les produits disponibles")   
             products.forEach(product => {
                 console.log(product.id, "-",product.name,"/",product.EUR_price/100,"/",product.orders_counter)
             });
+            return callback(null, products)
         } catch (error) {
-            console.log('Erreur parking du fichier products')
+           return callback(error)
         }   
     })
 }
 
-getAllProducts();
+function orderProductById(id) {
+    getAllProducts((err, products) => {
+        var link;
+        products.forEach(product => {
+             if (product.id == id) {
+                product.orders_counter++;
+                link = product.file_link;
+            }
+        });
+        let new_products = {"products" : products}
+        fs.writeFile(path.join(__dirname,"products.json"), JSON.stringify(new_products, null, 4), (err) => {
+            if (err) throw err;
+            console.log('Commande terminée! Voici votre fichier',link);
+        });
+    })
+    }
+
+
+//getAllProducts();
+//orderProductById("produit2")
+
+var rl = readline.createInterface({'input': process.stdin, output: process.stdout, terminal:false })
+
+rl.on('line', function (line) {
+    if (line.includes("i want product")) {
+        let line_info = line.split(" ")
+        let product_id = line_info[line_info.length-1];
+        orderProductById(product_id)
+    }
+})
