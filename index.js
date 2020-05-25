f8ca2f54656272f35b04c14d2d4d7f236bcd0bd9
Dominique DUPLAN <dominique.duplan@batiactugroupe.com>
2020-05-25 13:38:38 +0200

    try catch parsing & passage en centimes

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ let fs = require("fs")
 let path = require("path")
 
 fs.readFile(path.join(__dirname,"products.json"), "utf8", (err,contents) => {
-  products_list = JSON.parse(contents) ;
-  console.log(products_list['products'])
+    if (err) return console.log('Erreur de lecture du fichier poducts : ', err)
+    try {
+        var products = JSON.parse(contents).products ;    
+        console.log(products)
+    } catch (error) {
+        console.log('Erreur parking du fichier products')
+    }   
 })
